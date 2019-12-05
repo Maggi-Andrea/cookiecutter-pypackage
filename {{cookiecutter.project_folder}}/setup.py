63bf06a35471637bc0df600a782136e4e169fc1b
Andrea Maggi <andrea@maggicontrols.com>
2019-12-05 18:03:31 +0100

    added project_folder

--- {{cookiecutter.project_folder}}/setup.py
+++ {{cookiecutter.project_folder}}/setup.py
@@ -4,6 +4,8 @@
 
 from setuptools import setup, find_packages
 
+from docs.source import conf
+
 with open('README.rst') as readme_file:
     readme = readme_file.read()
 
@@ -25,9 +27,11 @@ test_requirements = [{%- if cookiecutter.use_pytest == 'y' %}'pytest>=3',{%- end
 } %}
 
 setup(
+    name=conf.project,
+    version=conf.release,
     author="{{ cookiecutter.full_name.replace('\"', '\\\"') }}",
     author_email='{{ cookiecutter.email }}',
-    python_requires='>=3.5',
+    python_requires='>=3.7',
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Developers',
@@ -36,8 +40,6 @@ setup(
 {%- endif %}
         'Natural Language :: English',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
     ],
@@ -56,12 +58,10 @@ setup(
     long_description=readme + '\n\n' + history,
     include_package_data=True,
     keywords='{{ cookiecutter.project_slug }}',
-    name='{{ cookiecutter.project_slug }}',
     packages=find_packages(include=['{{ cookiecutter.project_slug }}', '{{ cookiecutter.project_slug }}.*']),
     setup_requires=setup_requirements,
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/{{ cookiecutter.github_username }}/{{ cookiecutter.project_slug }}',
-    version='{{ cookiecutter.version }}',
     zip_safe=False,
 )
